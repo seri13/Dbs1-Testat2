3d3e5b0e0641a177add224d7146c4549bb914276
Lukas Schneider <Lukas Schneider>
2015-11-02 23:29:18 +0100

    added additional column to viewer distribution to debug

--- SQL/4_constraints.sql
+++ SQL/4_constraints.sql
@@ -3,48 +3,48 @@
  * TODO: -ON DELETE CASCADE?
  *       -Additional Constraints?
  */
- ALTER TABLE Transfers 
- ADD CONSTRAINT fk_tBuyer 
+ ALTER TABLE Transfers
+ ADD CONSTRAINT fk_tBuyer
     FOREIGN KEY (Käufer) REFERENCES Clubs (ClubId)
  ;
- ALTER TABLE Transfers 
+ ALTER TABLE Transfers
  ADD CONSTRAINT fk_tSeller
     FOREIGN KEY (Verkäufer) REFERENCES Clubs (ClubId)
  ;
- ALTER TABLE Transfers 
- ADD CONSTRAINT fk_tEmployee 
+ ALTER TABLE Transfers
+ ADD CONSTRAINT fk_tEmployee
     FOREIGN KEY (Angestellter) REFERENCES Angestellten (AngId)
  ;
-  
- ALTER TABLE Begegnungnen 
- ADD CONSTRAINT fk_gmHome 
+
+ ALTER TABLE Begegnungnen
+ ADD CONSTRAINT fk_gmHome
     FOREIGN KEY (Heim)  REFERENCES Clubs (ClubId)
  ;
- ALTER TABLE Begegnungnen 
- ADD CONSTRAINT fk_gmAway 
+ ALTER TABLE Begegnungnen
+ ADD CONSTRAINT fk_gmAway
     FOREIGN KEY (Gast)  REFERENCES Clubs (ClubId)
  ;
- ALTER TABLE Begegnungnen 
+ ALTER TABLE Begegnungnen
  ADD CONSTRAINT fk_gmStadium
     FOREIGN KEY (Austragungsort) REFERENCES Clubs (Stadion
  ;
- ALTER TABLE Begegnungnen 
- ADD CONSTRAINT fk_gmWinner 
+ ALTER TABLE Begegnungnen
+ ADD CONSTRAINT fk_gmWinner
     FOREIGN KEY (Gewinner)  REFERENCES Clubs (ClubId)
  ;
- 
- ALTER TABLE Zuschauer 
- ADD CONSTRAINT fk_vFavClub 
+
+ ALTER TABLE Zuschauer
+ ADD CONSTRAINT fk_vFavClub
     FOREIGN KEY (Lieblingsverein) REFERENCES Clubs (ClubId)
     ON DELETE CASCADE
  ;
- 
- ALTER TABLE Zuschauerverteilungen 
+
+ ALTER TABLE Zuschauerverteilungen
  ADD CONSTRAINT fk_vdGame
     FOREIGN KEY (Spiel) REFERENCES Begegnungnen (BegegnungsId)
  ;
- 
- ALTER TABLE Zuschauerverteilungen 
- ADD CONSTRAINT fk_vdViewer 
+
+ ALTER TABLE Zuschauerverteilungen
+ ADD CONSTRAINT fk_vdViewer
     FOREIGN KEY (Zuschauer) REFERENCES Zuschauer (ZuschauerId)
  ;
